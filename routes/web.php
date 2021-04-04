0fcae8679ee4ca28a22fbc3ef91717e226c63501
Trunghuyphan <hoang.hac1999@gmail.com>
2021-04-04 10:42:03 +0700

    edit_infoi chua xong

--- routes/web.php
+++ routes/web.php
@@ -21,22 +21,24 @@ Route::get('/all-product', 'ProductController@AllProduct')->name('allproduct');
 Route::get('/search-product', 'ProductController@Search')->name('search');
 Route::get('/product-details/{product_id}', 'ProductController@DetailsProduct')->name('details');
 /*Đăng ký*/
-Route::post('/dangky','HomeController@dangky')->name('dangky');
+Route::post('/dangky', 'HomeController@dangky')->name('dangky');
 /*Đăng nhập*/
-Route::post('/dangnhap','HomeController@dangnhap')->name('dangnhap');
+Route::post('/dangnhap', 'HomeController@dangnhap')->name('dangnhap');
 /*Đăng xuất*/
-    Route::get('logout', function(){
-        session()->flush();
-        // $tinh = DB::select("SELECT Tên FROM tinh");
-        return redirect(route("home"));
-    })->name('logout');
+Route::get('logout', function () {
+    session()->flush();
+    // $tinh = DB::select("SELECT Tên FROM tinh");
+    return redirect(route("home"));
+})->name('logout');
+// Chỉnh sua info KH
+Route::get('/thongtin/2', 'HomeController@Chinhsuainfo')->name('Chinhsuathongtin');
 /* giỏ hàng */
 
-Route::post('/save-cart','GioHangController@save_cart');
-Route::post('/add-cart-ajax','GioHangController@add_cart_ajax');
-Route::get('/show-cart','GioHangController@show_cart')->name('giohang');
-Route::post('/update-cart-quantity','GioHangController@update_cart_quantity');
-Route::post('/update-cart','GioHangController@update_cart');
-Route::get('/delete-to-cart/{rowId}','GioHangController@delete_to_cart');
-Route::get('/del-product/{session_id}','GioHangController@delete_product');
-Route::get('/del-all-product','GioHangController@delete_all_product');
+Route::post('/save-cart', 'GioHangController@save_cart');
+Route::post('/add-cart-ajax', 'GioHangController@add_cart_ajax');
+Route::get('/show-cart', 'GioHangController@show_cart')->name('giohang');
+Route::post('/update-cart-quantity', 'GioHangController@update_cart_quantity');
+Route::post('/update-cart', 'GioHangController@update_cart');
+Route::get('/delete-to-cart/{rowId}', 'GioHangController@delete_to_cart');
+Route::get('/del-product/{session_id}', 'GioHangController@delete_product');
+Route::get('/del-all-product', 'GioHangController@delete_all_product');
