e94983d7959476cef6f7467c96ab12c8aede3b9a
phucs2612 <phuclx123456@gmail.com>
2021-04-05 00:47:37 +0700

    liên kết menu đến danh sách sản phẩm

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,12 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Customer;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class CategoryController extends Controller
 {
     //
 
+    public function Category($category_product)
+    {
+        $category_pro = DB::select("SELECT * FROM tbl_product t, tbl_category_product c where t.category_id=$category_product AND t.category_id=c.category_id");
+        return view('product.categoryproducts')->with('category_pro', $category_pro); //1
+
+    }
 }
