94e34035c9e5ca6297fe98ea09673d8f3c2254ad
Trunghuyphan <hoang.hac1999@gmail.com>
2021-04-10 19:05:52 +0700

    xong detail

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Customer;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
 class CategoryController extends Controller
 {
     //
